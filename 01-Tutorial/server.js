ef5bd7cdcd7cee4c822c1c5cea9d12ad492b7407
LATEEF-hub <latifkk@krok.edu.ua>
2024-01-29 05:33:02 +0000

    Initial commit

---
+++ 01-Tutorial/server.js
@@ -0,0 +1,6 @@
+// You should already know HTML, CSS, JAVASCRIPT
+// This is Node JS beginner not 
+// In Node there Global object instead of normal objects. 
+
+console.log("Welcome to your world");
+console.log(global);
